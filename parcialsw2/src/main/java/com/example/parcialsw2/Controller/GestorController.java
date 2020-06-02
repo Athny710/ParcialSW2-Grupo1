73c93dbaa547aa02089dec75fdb2c0ee275b2eac
Anthony Rojas <a20145976@pucp.pe>
2020-06-02 16:42:51 -0500

    12

--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/GestorController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/GestorController.java
@@ -5,6 +5,10 @@ import com.example.parcialsw2.entity.Usuario;
 import com.example.parcialsw2.repository.ProductoRepository;
 import com.example.parcialsw2.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,6 +32,20 @@ public class GestorController {
     public String paginaInicio(Model model){
         model.addAttribute("lista", productoRepository.findAll());
         return "index";
+    }
+
+    @GetMapping("/image/{id}")
+    public ResponseEntity<byte[]> mostrarImagen(@PathVariable("id") int id){
+        Optional<Producto> opt = productoRepository.findById(id);
+        if(opt.isPresent()){
+            Producto p = opt.get();
+            byte[] imagenComoBytes = p.getFoto();
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.parseMediaType(p.getFotocontenttype()));
+            return new ResponseEntity<>(imagenComoBytes, httpHeaders, HttpStatus.OK);
+        }else{
+            return null;
+        }
     }
 
     @GetMapping("/nuevo")
