20f66697ec80fb499e5953d08faeaa9b3242f56f
Anthony Rojas <a20145976@pucp.pe>
2020-06-02 12:59:14 -0500

    1

---
+++ parcialsw2/src/main/java/com/example/parcialsw2/entity/ProductoSel.java
@@ -0,0 +1,62 @@
+package com.example.parcialsw2.entity;
+
+import org.hibernate.annotations.Tables;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "producto_seleccionado")
+public class ProductoSel {
+
+    @Id
+    private int idproductoseleccionado;
+    private int cantidad;
+    @ManyToOne
+    @JoinColumn(name = "idproducto")
+    private Producto producto;
+    @ManyToOne
+    @JoinColumn(name = "idusuarios")
+    private Usuario usuario;
+    private String codigo;
+
+
+    public int getIdproductoseleccionado() {
+        return idproductoseleccionado;
+    }
+
+    public void setIdproductoseleccionado(int idproductoseleccionado) {
+        this.idproductoseleccionado = idproductoseleccionado;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ parcialsw2/src/main/java/com/example/parcialsw2/entity/Usuario.java
@@ -0,0 +1,85 @@
+package com.example.parcialsw2.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    private int idusuarios;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String correo;
+    private String contrasenha;
+    private String rol;
+    private int activo;
+
+
+    public int getIdusuarios() {
+        return idusuarios;
+    }
+
+    public void setIdusuarios(int idusuarios) {
+        this.idusuarios = idusuarios;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public void setActivo(int activo) {
+        this.activo = activo;
+    }
+}
