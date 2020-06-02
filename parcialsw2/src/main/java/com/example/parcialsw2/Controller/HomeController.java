cf59410fdd89ae879e0c86ace4544f79fd3e1d1a
Julio Carrion <a20161448@pucp.edu.pe>
2020-06-02 17:20:18 -0500

    gaaaaaaaaa

--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/HomeController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.parcialsw2.Controller;
 
+import com.example.parcialsw2.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    ProductoRepository productoRepository;
 
-    @GetMapping(value = {"","/inicio"})
-    public String index(){
+    @GetMapping(value = {"","/list"})
+    public String index(Model model){
+        model.addAttribute("lista", productoRepository.findAll());
         return "index";
+
     }
 }
