5a2209ffa54222e511c46075805da4f47546d6f2
Julio Carrion <a20161448@pucp.edu.pe>
2020-06-02 17:51:54 -0500

    quiero llorar

--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/HomeController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/HomeController.java
@@ -1,10 +1,18 @@
 package com.example.parcialsw2.Controller;
 
+import com.example.parcialsw2.entity.Producto;
 import com.example.parcialsw2.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -14,7 +22,21 @@ public class HomeController {
     @GetMapping(value = {"","/list"})
     public String index(Model model){
         model.addAttribute("lista", productoRepository.findAll());
-        return "index";
+        return "index2";
 
+    }
+
+    @GetMapping("/image/{id}")
+    public ResponseEntity<byte[]> mostrarImagen(@PathVariable("id") int id){
+        Optional<Producto> opt = productoRepository.findById(id);
+        if(opt.isPresent()){
+            Producto p = opt.get();
+            byte[] imagenComoBytes = p.getFoto();
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.parseMediaType(p.getFotocontenttype()));
+            return new ResponseEntity<>(imagenComoBytes, httpHeaders, HttpStatus.OK);
+        }else{
+            return null;
+        }
     }
 }
