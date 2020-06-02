de764f21611cbc312bf3efaea88a22b05a3f5658
Anthony Rojas <a20145976@pucp.pe>
2020-06-02 12:09:36 -0500

    baseParcial

---
+++ parcialsw2/src/main/java/com/example/parcialsw2/Parcialsw2Application.java
@@ -0,0 +1,13 @@
+package com.example.parcialsw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcialsw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parcialsw2Application.class, args);
+    }
+
+}
---
+++ parcialsw2/src/test/java/com/example/parcialsw2/Parcialsw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialsw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcialsw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
