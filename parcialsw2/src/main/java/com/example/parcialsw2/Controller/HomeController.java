8fba5ec7c2f7a622c805447102a4c6f7ffc723c1
Julio Carrion <a20161448@pucp.edu.pe>
2020-06-03 12:05:16 -0500

    TENGO MIEDO7

--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/HomeController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.example.parcialsw2.Controller;
 
 import com.example.parcialsw2.entity.Usuario;
+import com.example.parcialsw2.repository.PaginationRepository;
 import com.example.parcialsw2.repository.UsuarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +15,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import com.example.parcialsw2.entity.Producto;
 import com.example.parcialsw2.repository.ProductoRepository;
@@ -30,13 +37,46 @@ public class HomeController {
     ProductoRepository productoRepository;
     @Autowired
     UsuarioRepository usuarioRepository;
+    @Autowired
+    PaginationRepository paginationRepository;
 
     @GetMapping(value = {"","/list"})
-    public String index(Model model){
-        model.addAttribute("lista", productoRepository.findAll());
-        return "index2";
+    public String index(Model model, @RequestParam(defaultValue = "0") int page){
+        if(page <= 0){
+            Pageable pageable = PageRequest.of(page,7);
+            Page<Producto> lista1 = paginationRepository.findAll(pageable);
+            int totalPages = lista1.getTotalPages();
+            List<Integer> paginas = new ArrayList<>();
+
+            for(int i=1; i<=totalPages; i++){
+                paginas.add(i);
+            }
+
+            List<Producto> lista = lista1.getContent();
+            model.addAttribute("page",page);
+            model.addAttribute("lista",lista);
+            model.addAttribute("paginas",paginas);
+            return "index2";
+        }else{
+            Pageable pageable = PageRequest.of(page -1,7);
+            Page<Producto> lista1 = paginationRepository.findAll(pageable);
+            int totalPages = lista1.getTotalPages();
+            List<Integer> paginas = new ArrayList<>();
+
+            for(int i=1; i<=totalPages; i++){
+                paginas.add(i);
+            }
+
+            List<Producto> lista = lista1.getContent();
+            model.addAttribute("page",page);
+            model.addAttribute("lista",lista);
+            model.addAttribute("paginas",paginas);
+            return "index2";
+        }
 
     }
+
+
     @GetMapping("/vermas")
     public String vermas(@RequestParam("id") int id, Model model, HttpSession session){
         Optional<Producto> opt = productoRepository.findById(id);
--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/RegistradoController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/RegistradoController.java
@@ -3,9 +3,13 @@ package com.example.parcialsw2.Controller;
 import com.example.parcialsw2.entity.Producto;
 import com.example.parcialsw2.entity.ProductoSel;
 import com.example.parcialsw2.entity.Usuario;
+import com.example.parcialsw2.repository.PaginationRepository;
 import com.example.parcialsw2.repository.ProdSelRepository;
 import com.example.parcialsw2.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,10 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 @RequestMapping("/registrado")
@@ -29,11 +30,43 @@ public class RegistradoController {
     ProductoRepository productoRepository;
     @Autowired
     ProdSelRepository prodSelRepository;
+    @Autowired
+    PaginationRepository paginationRepository;
 
     @GetMapping(value = {"","/list"})
-    public String paginaInicio(Model model){
-        model.addAttribute("lista", productoRepository.findAll());
-        return "index";
+    public String index(Model model, @RequestParam(defaultValue = "0") int page){
+        if(page <= 0){
+            Pageable pageable = PageRequest.of(page,7);
+            Page<Producto> lista1 = paginationRepository.findAll(pageable);
+            int totalPages = lista1.getTotalPages();
+            List<Integer> paginas = new ArrayList<>();
+
+            for(int i=1; i<=totalPages; i++){
+                paginas.add(i);
+            }
+
+            List<Producto> lista = lista1.getContent();
+            model.addAttribute("page",page);
+            model.addAttribute("lista",lista);
+            model.addAttribute("paginas",paginas);
+            return "index";
+        }else{
+            Pageable pageable = PageRequest.of(page -1,7);
+            Page<Producto> lista1 = paginationRepository.findAll(pageable);
+            int totalPages = lista1.getTotalPages();
+            List<Integer> paginas = new ArrayList<>();
+
+            for(int i=1; i<=totalPages; i++){
+                paginas.add(i);
+            }
+
+            List<Producto> lista = lista1.getContent();
+            model.addAttribute("page",page);
+            model.addAttribute("lista",lista);
+            model.addAttribute("paginas",paginas);
+            return "index";
+        }
+
     }
 
     @GetMapping("/image/{id}")
