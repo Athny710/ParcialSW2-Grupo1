37125db3c8f403009d0fb2a69dd3bf135158f8b5
Julio Carrion <a20161448@pucp.edu.pe>
2020-06-02 19:23:53 -0500

    AHORA SI QUIERO  LLORAR 2

--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/RegistradoController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/RegistradoController.java
@@ -1,6 +1,9 @@
 package com.example.parcialsw2.Controller;
 
 import com.example.parcialsw2.entity.Producto;
+import com.example.parcialsw2.entity.ProductoSel;
+import com.example.parcialsw2.entity.Usuario;
+import com.example.parcialsw2.repository.ProdSelRepository;
 import com.example.parcialsw2.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -9,11 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +25,8 @@ public class RegistradoController {
 
     @Autowired
     ProductoRepository productoRepository;
+    @Autowired
+    ProdSelRepository prodSelRepository;
 
     @GetMapping(value = {"","/list"})
     public String paginaInicio(Model model){
@@ -42,6 +47,37 @@ public class RegistradoController {
             return null;
         }
     }
+    @GetMapping("/carrito")
+    public String añadirCarrito(@RequestParam("id") int id, HttpSession session, RedirectAttributes attr){
+        Optional<Producto> opt = productoRepository.findById(id);
+        if(opt.isPresent()){
+            Producto p = opt.get();
+            Usuario usuario = (Usuario) session.getAttribute("user");
+            List<ProductoSel> seleccionado = prodSelRepository.findByProductoAndAndUsuario(p,usuario);
 
+            if(seleccionado.isEmpty()){
+                 ProductoSel productoSel= new ProductoSel();
+                 productoSel.setUsuario(usuario);
+                 productoSel.setProducto(p);
+                 productoSel.setCantidad(1);
+                attr.addFlashAttribute("msg","Se agregó correctamente");
+                 prodSelRepository.save(productoSel);
+            }else{
+                ProductoSel pro =seleccionado.get(0);
+                int cantidad = 1+ pro.getCantidad();
+                if(cantidad >= 4){
+                    attr.addFlashAttribute("msg1","Solo se permiten 4 unidades por producto");
+                    return "redirect:/list";
+                }else{
+                    pro.setCantidad(cantidad);
+                    attr.addFlashAttribute("msg","Se agregó correctamente");
+                }
+
+            }
+            return "redirect:/list";
+        }else{
+            return "redirect:/list";
+        }
+    }
 
 }
--- parcialsw2/src/main/java/com/example/parcialsw2/repository/ProdSelRepository.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/repository/ProdSelRepository.java
@@ -1,7 +1,13 @@
 package com.example.parcialsw2.repository;
 
+import com.example.parcialsw2.entity.Producto;
 import com.example.parcialsw2.entity.ProductoSel;
+import com.example.parcialsw2.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdSelRepository extends JpaRepository<ProductoSel, Integer> {
+
+    List<ProductoSel> findByProductoAndAndUsuario(Producto pro, Usuario usu);
 }
