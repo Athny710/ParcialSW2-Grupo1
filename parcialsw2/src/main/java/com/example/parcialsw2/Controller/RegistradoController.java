f3dd0a04b35f203588c006e2ea70d24ae939e3a4
Anthony Rojas <a20145976@pucp.pe>
2020-06-02 19:36:46 -0500

    17

--- parcialsw2/src/main/java/com/example/parcialsw2/Controller/RegistradoController.java
+++ parcialsw2/src/main/java/com/example/parcialsw2/Controller/RegistradoController.java
@@ -1,6 +1,7 @@
 package com.example.parcialsw2.Controller;
 
 import com.example.parcialsw2.entity.Producto;
+import com.example.parcialsw2.repository.ProdSelRepository;
 import com.example.parcialsw2.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -9,10 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -43,5 +42,49 @@ public class RegistradoController {
         }
     }
 
+    @GetMapping("/vistacheck")
+    public String vistacheck(){
+        return "registrado/checkout";
+    }
+    @PostMapping("/checkout")
+    public String checkout(@RequestParam("numero") String num, Model model){
+        if(verificarTarjeta(num)){
+            model.addAttribute("msg", "tarjeta valida");
+            return "registrado/checkout";
+        }else{
+            model.addAttribute("msg", "tarjeta invlida");
+            return "registrado/checkout";
+        }
+
+    }
+
+
+    public boolean verificarTarjeta(String numero){
+        char[] caracteres = numero.toCharArray();
+        int[] carac2 = new int[14];
+        int ultimo = Character.getNumericValue(caracteres[15]);
+        int suma=0;
+
+        for(int i=0, j=14; i<14; i++, j--){
+            carac2[i] = Character.getNumericValue(caracteres[j]);
+        }
+        for (int i=0; i<7; i++){
+            carac2[i]=carac2[i]*2;
+            i++;
+        }
+        for (int i=0; i<14; i++){
+            if(carac2[i]>=9){
+                carac2[i]=carac2[i]-9;
+            }
+        }
+        for (int i=0; i<14; i++){
+            suma= suma + carac2[i];
+        }
+        if(suma%10==ultimo){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 }
